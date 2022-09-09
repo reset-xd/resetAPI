96f099e53540fa0a065ef0f53c1cd5f485aa4187
Reset <resetwastakenwastaken@gmail.com>
2022-09-09 21:59:29 +0530

    issue on main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import os
 
 if __name__ == "__main__":
     # uvicorn.run("app:app",host="0.0.0.0",port=5000, reload=False)
-    # uvicorn.run("app:app",host="0.0.0.0", reload=False)
-    uvicorn.run("app:app", host="localhost",port=2000, reload=True)
+    uvicorn.run("app:app",host="0.0.0.0", reload=False)
+    # uvicorn.run("app:app", host="localhost",port=2000, reload=True)
